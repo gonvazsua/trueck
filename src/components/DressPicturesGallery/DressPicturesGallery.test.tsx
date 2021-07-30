2a84fa7190b7759b194c8b179e6aa73fb457d47b
Gonzalo Vazquez Suarez <gonzalo.vazquez@thoughtworks.com>
2021-07-30 14:03:40 +0200

    Adds carousel to dress gallery

--- src/components/DressPicturesGallery/DressPicturesGallery.test.tsx
+++ src/components/DressPicturesGallery/DressPicturesGallery.test.tsx
@@ -14,7 +14,7 @@ describe('DressForGrid test', () => {
         );
     };
 
-    test('Should render main picture from the beginning', async () => {
+    test('Should render pictures in carousel', async () => {
 
         const dressPictures: DressPicture[] = [
             {url:'https://borow.es/wp-content/uploads/2021/04/Alice-Olicia-vestido-tirantes-midi-rojo-azul-1.jpeg', main:false},
@@ -23,24 +23,7 @@ describe('DressForGrid test', () => {
 
         renderComponent(dressPictures);
 
-        expect(screen.getByTestId('DressPicturesGallery-mainPicture')).toHaveAttribute('src', 'https://borow.es/wp-content/uploads/2021/04/Alice-Olicia-vestido-tirantes-midi-rojo-azul-2.jpeg');
-    });
-
-    test('should render selected picture when clicking on it', async () => {
-        const dressPicture1: DressPicture = {url:'https://borow.es/wp-content/uploads/2021/04/Alice-Olicia-vestido-tirantes-midi-rojo-azul-1.jpeg', main:false, name: 'Picture1'};
-        const dressPicture2: DressPicture = {url:'https://borow.es/wp-content/uploads/2021/04/Alice-Olicia-vestido-tirantes-midi-rojo-azul-2.jpeg', main:true, name: 'Picture2'};
-        const dressPictures: DressPicture[] = [
-            dressPicture1,
-            dressPicture2
-        ];
-
-        renderComponent(dressPictures);
-
-        act(() => {
-             fireEvent.click(screen.getByTestId('DressPicturesGallery-picture-' + dressPicture1.name));
-        });
-
-        await waitFor(() => expect(screen.getByTestId('DressPicturesGallery-mainPicture')).toHaveAttribute('src', 'https://borow.es/wp-content/uploads/2021/04/Alice-Olicia-vestido-tirantes-midi-rojo-azul-1.jpeg'));
+        expect(screen.getByTestId('DressPicturesGallery-picture-' + dressPictures[0].name)).toHaveAttribute('src', 'https://borow.es/wp-content/uploads/2021/04/Alice-Olicia-vestido-tirantes-midi-rojo-azul-1.jpeg');
     });
 
 });
--- src/components/DressPicturesGallery/DressPicturesGallery.tsx
+++ src/components/DressPicturesGallery/DressPicturesGallery.tsx
@@ -1,11 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Grid from "@material-ui/core/Grid";
 import {DressPicture} from "../../api/dress/dressAPI";
-import ImageList from '@material-ui/core/ImageList';
-import ImageListItem from '@material-ui/core/ImageListItem';
-import ImageListItemBar from '@material-ui/core/ImageListItemBar';
 import {makeStyles} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
+import Carousel from "react-material-ui-carousel";
 
 export interface DressDetailsProps {
     dressPictures: DressPicture[] | null | undefined;
@@ -14,69 +12,37 @@ export interface DressDetailsProps {
 const DressPicturesGallery = (props: DressDetailsProps): JSX.Element => {
 
     const classes = useStyles();
-    const { dressPictures } = props;
-    const [mainPicture, setMainPicture] = useState<DressPicture | null>(null);
-
-    useEffect(() => {
-        const mainPictures = dressPictures?.filter(dp => dp.main === true);
-        if(mainPictures && mainPictures.length > 0) {
-            setMainPicture(mainPictures[0]);
-        } else if(dressPictures && dressPictures.length > 0) {
-            setMainPicture(dressPictures[0]);
-        }
-    }, [dressPictures]);
-
-    const handlePictureClick = (selectedPicture: DressPicture) => {
-        setMainPicture(selectedPicture);
-    };
+    const {dressPictures} = props;
 
     return (
         <Grid container spacing={3}>
             <Grid item lg={12}>
-                <img src={mainPicture?.url} data-testid={'DressPicturesGallery-mainPicture'}/>
+                <Carousel autoPlay={false}>
+                    {
+                        dressPictures?.map((item, i) =>
+                            <img key={'DressPicturesGallery-picture-' + item.name}
+                                 src={item.url}
+                                 alt={item.url}
+                                 className={classes.picture}
+                                 data-testid={'DressPicturesGallery-picture-' + item.name}
+                            />)
+                    }
+                </Carousel>
             </Grid>
             <Grid item lg={12}>
                 <Typography align={"center"}>
                     {dressPictures?.length} imágenes disponibles para este look
                 </Typography>
             </Grid>
-            <Grid item lg={12}>
-                <ImageList rowHeight={250} gap={1} className={classes.imageList}>
-                    {dressPictures?.map((item) => (
-                        <ImageListItem key={item.url}>
-                            <img src={item.url} alt={item.url} data-testid={'DressPicturesGallery-picture-' + item.name} onClick={() => handlePictureClick(item)}/>
-                            <ImageListItemBar
-                                position="top"
-                                actionPosition="left"
-                                className={classes.titleBar}
-                            />
-                        </ImageListItem>
-                    ))}
-                </ImageList>
-            </Grid>
         </Grid>
     );
 };
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden',
-        backgroundColor: theme.palette.background.paper,
-    },
-    imageList: {
+    picture: {
         width: '100%',
-        height: 450,
-        // Promote the list into its own layer in Chrome. This cost memory, but helps keep FPS high.
-        transform: 'translateZ(0)',
-    },
-    titleBar: {
-        background:
-            'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
-            'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-    },
+        height: 'auto',
+    }
 }));
 
 export default DressPicturesGallery;
